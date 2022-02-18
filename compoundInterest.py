a160ca1217c72c88a7751d78838102ed0d4d7d54
Hunter Hughes <Hunter.Hughes@vu.com>
2022-02-18 12:42:15 -0600

    Initial Commit

--- compoundInterest.py
+++ compoundInterest.py
@@ -5,7 +5,26 @@ def calculateCompoundInterest():
  client_one_principal = float(input("Principle (amount): "))
  client_one_time =      float(input("Time:               "))
  client_one_rate =      float(input("Rate:               "))
- #print("Compound Interest: "+str(intrest))
+
+ amount = (client_one_principal*(1+(client_one_rate/100))**client_one_time)
+ interest = round((amount - client_one_principal),2)
+ print("Compound Interest: "+str(interest)+"\n")
+
+ client_two_principal = float(input("Principle (amount): "))
+ client_two_time =      float(input("Time:               "))
+ client_two_rate =      float(input("Rate:               "))
+
+ amount = (client_two_principal*(1+(client_two_rate/100))**client_two_time)
+ interest = round((amount - client_two_principal),2)
+ print("Compound Interest: "+str(interest)+"\n")
+
+ client_three_principal = float(input("Principle (amount): "))
+ client_three_time =      float(input("Time:               "))
+ client_three_rate =      float(input("Rate:               "))
+
+ amount = (client_three_principal*(1+(client_three_rate/100))**client_three_time)
+ interest = round((amount - client_three_principal),2)
+ print("Compound Interest: "+str(interest))
 
     # end assignment
 
